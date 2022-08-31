23573655196e80e17d664ef9fe76f0d708f86eaf
eks05 <65056583+eks05@users.noreply.github.com>
2022-08-31 21:38:54 +0900

    hi

--- app/src/main/java/com/example/asd/Studymode.kt
+++ app/src/main/java/com/example/asd/Studymode.kt
@@ -1,12 +1,17 @@
 package com.example.asd
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class Studymode : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.studymode)
+        var int_count = 1
+
+        //타이머 객체 선언
+        var t_timer = Timer()
     }
 }
