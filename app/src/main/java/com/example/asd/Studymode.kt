fb40892e4868cc7435c9296a7879fbb40054c300
seonghun <kimsh20502@gmail.com>
2022-09-15 18:05:15 +0900

    공부 시간 추가

--- app/src/main/java/com/example/asd/Studymode.kt
+++ app/src/main/java/com/example/asd/Studymode.kt
@@ -3,18 +3,24 @@ package com.example.asd
 import android.app.Activity
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.appsearch.AppSearchSchema
 import android.content.*
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.provider.CallLog
 import android.provider.Settings
 import android.telephony.ServiceState
 import android.telephony.TelephonyManager
 import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.WindowManager
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -22,6 +28,7 @@ import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil.setContentView
 import com.example.asd.getCode.get_Code
 import com.google.gson.GsonBuilder
+import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -35,7 +42,12 @@ class Studymode : AppCompatActivity() {
 
     private var time = 0
     private var timerTask: Timer? = null
+    private lateinit var currntTime: TextView
     private lateinit var studyRecord: TextView
+    private lateinit var studyTimeText: TextView
+    private lateinit var clockText:TextView
+    private lateinit var alert:TextView
+    private lateinit var fiveSecond: TextView
 
     var nfcTag: Tag? = null
     var nfcAdapter: NfcAdapter? = null
@@ -62,8 +74,13 @@ class Studymode : AppCompatActivity() {
             }
         }
 
-
+        currntTime = findViewById(R.id.currentTime)
+        clockText = findViewById(R.id.clock)
+        studyTimeText = findViewById(R.id.studyTime)
         studyRecord = findViewById(R.id.record)
+        alert = findViewById(R.id.alert)
+        fiveSecond = findViewById(R.id.fiveSecond)
+
         nfcAdapter = NfcAdapter.getDefaultAdapter(this)
         pendingIntent = PendingIntent.getActivity(this, 0, Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
         val intent: Intent = getIntent()
@@ -94,6 +111,26 @@ class Studymode : AppCompatActivity() {
 //            }
 //        })
     }
+    /*화면 풀스크린 띄우기*/
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        hideNavigationBar()
+    }
+    private fun hideNavigationBar() {
+        val decorView: View = this.window.decorView
+        val uiOptions: Int = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+        val timer = Timer()
+        val task: TimerTask = object : TimerTask() {
+            override fun run() {
+                runOnUiThread { decorView.systemUiVisibility = uiOptions }
+            }
+        }
+        timer.scheduleAtFixedRate(task, 1, 2)
+    }
 
     private fun studying() {
         val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
@@ -103,21 +140,68 @@ class Studymode : AppCompatActivity() {
                 toast("Do Not Disturb turned off")
             }
         }
+        /*detechedCheck = 1이면 스피커에 폰이 붙어있음/ 0이면 반대 */
+        var detechedCheck = 1
+        /*5초 내로 nfc가 붙는 지 체크*/
+        var fiveCount = 0
+
+        var backUpTime = 0
+        var backUpSec = 0
+        var backUpMin = 0
+        var backUpHour = 0
         timerTask = kotlin.concurrent.timer(period = 1000) {
             time++
-            val sec = time % 60
-            val min = time % 3600 /60
-            val hour = time / 3600
+            var sec = time % 60
+            var min = time % 3600 /60
+            var hour = time / 3600
             runOnUiThread {
                 val nfcCheck = detectDetached()
                 Log.e("nfcCheck", "$nfcCheck")
                 if (nfcCheck == false) {
-                    cancel()
-                    finish()
-                    offDND()
-
+                    if (detechedCheck == 1) {
+                        backUpTime = time
+                        backUpSec = sec
+                        backUpMin = min
+                        backUpHour = hour
+                        detechedCheck = 0
+                        fiveCount += 1
+                    }
+                    else if (detechedCheck == 0 && fiveCount < 15) {
+                        fiveCount += 1
+                    }
+                    else if(fiveCount >= 15) {
+                        cancel()
+                        finish()
+                        offDND()
+                    }
                 }
-                studyRecord.text = "%1$02d:%2$02d:%3$02d".format(hour, min, sec)
+                else if(nfcCheck == true && detechedCheck == 0) {
+                    fiveCount = 0
+                    detechedCheck = 1
+                    time = backUpTime
+                    sec = backUpSec
+                    min = backUpMin
+                    hour = backUpHour
+                }
+                if (detechedCheck == 0) {
+                    currntTime.setTextColor(Color.BLACK)
+                    clockText.setTextColor(Color.BLACK)
+                    studyTimeText.setTextColor(Color.BLACK)
+                    studyRecord.setTextColor(Color.BLACK)
+                    alert.setTextColor(Color.WHITE)
+                    alert.text = "공부시간을 이어가려면 15초 이내로 스피커에 폰을 다시 올려주세요."
+                    fiveSecond.setTextColor(Color.RED)
+                    fiveSecond.text = "${16 - fiveCount}"
+                }
+                else {
+                    currntTime.setTextColor(Color.WHITE)
+                    clockText.setTextColor(Color.WHITE)
+                    studyTimeText.setTextColor(Color.WHITE)
+                    studyRecord.setTextColor(Color.WHITE)
+                    alert.setTextColor(Color.BLACK)
+                    fiveSecond.setTextColor(Color.BLACK)
+                    studyRecord.text = "%1$02d:%2$02d:%3$02d".format(hour, min, sec)
+                }
             }
         }
     }
@@ -131,19 +215,16 @@ class Studymode : AppCompatActivity() {
     }
 
     fun detectDetached(): Boolean{
-        val result = nfcAdapter?.ignore(nfcTag, 10, NfcAdapter.OnTagRemovedListener {  }, Handler())
+        val result = nfcAdapter?.ignore(
+            nfcTag,
+            1000,
+            NfcAdapter.OnTagRemovedListener  {},
+            Handler())
         if (result == null) {
             return false
         }
         return result
     }
-
-    /*뒤로 가기 버튼 막기*/
-    override fun onBackPressed() {
-//        super.onBackPressed()
-    }
-
-
     //DND
     // Method to check notification policy access status
     private fun checkNotificationPolicyAccess(notificationManager:NotificationManager):Boolean{
@@ -182,5 +263,9 @@ class Studymode : AppCompatActivity() {
         }
     }
 
+    /*홈, 뒤로가기 등의 버튼 막기*/
+    override fun onBackPressed() {
+//        super.onBackPressed()
+    }
 }
 
